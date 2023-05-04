7df7d8ba6d9f5b5950d90b6aeec88b8fcb634af2
kurosiko <101198724+kurosiko@users.noreply.github.com>
2023-05-05 00:37:39 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-window.onload = function(){
-    return TEST();
-}
-const imgs = [
+const images = [
     "https://images.unsplash.com/photo-1677868902699-0c5fb02d85ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
     "https://images.unsplash.com/photo-1682687218608-5e2522b04673?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80",
     "https://images.unsplash.com/photo-1682918917006-e78e1dab7dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
@@ -12,20 +9,17 @@ const imgs = [
     "https://images.unsplash.com/photo-1682961941145-e73336a53bc6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
     ]
 let count = -1;
-function TEST(){
+function ImageC(){
     count++
-    if (count > imgs.length - 1) count = 0;
-    cover.src = imgs[count]
-    console.log(count)
-    setTimeout("TEST()",4000)
+    if (count > images.length - 1) count = 0;
+    cover.src = images[count]
+    setTimeout("ImageC()",4000)
 }
-var element = document.getElementById("cover");
-element.addEventListener("mousemove", function(e) {
-var rect = element.getBoundingClientRect(); // 要素の位置とサイズを取得
-var x = e.clientX - (rect.left+rect.width); // x座標を計算
-var y = e.clientY - (rect.top+rect.height); // y座標を計算
-var heading = document.getElementById("test"); // h1要素を取
-var heading2 = document.getElementById("test2"); // h1要素を取得
+let fream = document.getElementById("cover");
+fream.addEventListener("mousemove", function(e) {
+let rect = fream.getBoundingClientRect(); // 要素の位置とサイズを取得
+let textX = document.getElementById("testX"); // h1要素を取
+let textY = document.getElementById("testY"); // h1要素を取得
 relativeX = e.clientX-rect.left-((rect.right-rect.left)/2);
 relativeY = e.clientY-rect.top-((rect.bottom-rect.top)/2);
 if (relativeX >= 100) relativeX = 100;
@@ -34,8 +28,12 @@ if (relativeY >= 100) relativeY = 100;
 if (relativeY <= -100) relativeY = -100;
 let moveX = relativeX*-0.1
 let moveY = relativeY*-0.1
-element.style.left = `${moveX}px`
-element.style.top = `${moveY}px`
-heading.textContent =`X:${Math.round(relativeX)}`
-heading2.textContent = `Y:${Math.round(relativeY)}`;
+fream.style.left = `${moveX}px`
+fream.style.top = `${moveY}px`
+textX.textContent =`X:${Math.round(relativeX)}`
+textY.textContent = `Y:${Math.round(relativeY)}`;
 });
+
+window.onload = function(){
+    return ImageC()
+}
